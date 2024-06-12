f7e3a3e4f502a5c8a67da73669f3d17726460341
Alexandr Demeshko <demeshk@yandex.ru>
2024-06-13 03:32:07 +0400

    first commit in this project

--- src/main/java/com/example/Feline.java
+++ src/main/java/com/example/Feline.java
@@ -11,15 +11,21 @@ public class Feline extends Animal implements Predator {
 
     @Override
     public String getFamily() {
+
         return "Кошачьи";
+
     }
 
     public int getKittens() {
+
         return getKittens(1);
+
     }
 
     public int getKittens(int kittensCount) {
+
         return kittensCount;
+
     }
 
 }
